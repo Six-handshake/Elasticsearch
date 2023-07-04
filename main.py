fd8d9870482a60443a470973aa7adad67a2268d6
Sobolev515 <alex.sobolev515@yandex.ru>
2023-07-04 15:05:09 +0500

    sample fastapi

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from datetime import datetime
-from elasticsearch import Elasticsearch
-# create a connection to Elasticsearch
-ELASTIC_PASSWORD = "sPPhehZErZpDMuQMe8ES"
-path = "D:/Downloads/elasticsearch-8.8.2-windows-x86_64/elasticsearch-8.8.2/config/certs/http_ca.crt"
-client = Elasticsearch(
-    "https://localhost:9200",
-    ca_certs=path,
-    basic_auth=("elastic", ELASTIC_PASSWORD)
-)
+from typing import Union
+from fastapi import FastAPI
 
-# Successful response!
-print(client.info())
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
