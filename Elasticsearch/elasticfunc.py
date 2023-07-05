28a2070a077fcc07a76dad79ae3c7e4f9983a9a2
Sobolev515 <alex.sobolev515@yandex.ru>
2023-07-05 19:16:50 +0500

    ElasticSearch More search Func

--- Elasticsearch/elasticfunc.py
+++ Elasticsearch/elasticfunc.py
@@ -3,23 +3,54 @@ from datetime import datetime
 
 es = Elasticsearch(hosts="http://46.48.3.74:9200")
 
-def GetDataId(doc_id : str):
-    resp = es.get(index='private_face',id=doc_id)
+
+def get_data_id(doc_id: str):
+    resp = es.get(index='private_face', id=doc_id)
     return resp["_source"]
 
-def GetDataText(full_text : str):
-    #TODO:analizator,tokenizator and search in elasticsearch
+
+def get_data_text(full_text: str):
+    # TODO:analizator,tokenizator and search in elasticsearch
     return "Later..."
 
-def GetDataTestInn(inn : str):
-    resp = es.search(index='private_face', query= {
+
+def get_data_test_inn(inn: str):
+    resp = es.search(index='private_face', query={
         "match": {
             "inn": inn
         }
     })
     return resp["hits"]["hits"][0]["_source"]
 
-#test
-#print(GetDataId(1))
-#print(GetDataText("Иванов"))
-#print(GetDataTestInn("7712345678900"))
+
+def get_data_all_info(inn="", fn="", sn="", p="", name=""):
+    resp = es.search(index='private_face', query={
+        "bool": {
+            "should": [
+                {
+                    "match": {"inn": inn}
+                },
+                {
+                    "match": {"firstname": fn}
+                },
+                {
+                    "match": {"secondname": sn}
+                },
+                {
+                    "match": {"patronymic": p}
+                },
+                {
+                    "match": {"name": name}
+                }
+            ]
+        }
+    })
+    return resp
+
+
+# test
+# print(GetDataId(1))
+# print(GetDataText("Иванов"))
+# print(GetDataTestInn("7712345678900"))
+print(GetDataAllInfo(inn="7712345678904", name="Best company"))
+print(GetDataAllInfo(inn="7712345678900"))
--- FastApi/api.py
+++ FastApi/api.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from Elasticsearch import  elasticfunc
+from typing import Union
 import json
 
 app = FastAPI()
@@ -32,6 +34,19 @@ async def add_test_data(test_data: dict) -> dict:
         "data": { "Test data added." }
     }
 
-@app.get("/test_data", tags=["test"])
-async def get_test_data() -> dict:
+@app.get("/test_data/{doc_id}", tags=["test"])
+async def get_test_data(doc_id : str) -> dict:
     return { "data": test }
+
+@app.get("/id/{doc_id}",tags=["main"])
+async def get_doc_for_id(doc_id : str, q: Union[str, None] = None) -> dict:
+    return elasticfunc.get_data_id(doc_id)
+
+@app.get("/test",tags=["main"])
+async def get_doc_for_text(q: Union[str, None] = None) -> dict:
+    print(q)
+    return elasticfunc.get_data_text("erewr")
+
+@app.get("/inn/{inn}",tags=["main"])
+async def get_doc_for_inn(inn : str, q: Union[str, None] = None) -> dict:
+    return elasticfunc.get_data_test_inn(inn)
